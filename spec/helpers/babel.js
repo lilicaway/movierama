c23ea0a05bebca46858e82ef043aeead86f567ef
Liliana <liliana.nu@gmail.com>
2019-07-09 00:23:26 +0200

    Fix process.env for the apiKey in tests.

--- spec/helpers/babel.js
+++ spec/helpers/babel.js
@@ -8,3 +8,11 @@ require.extensions['.css'] = () => {
 require.extensions['.svg'] = () => {
   return '';
 };
+
+// Global variables needed for api_key.ts.
+// The function getApiKey() is not really needed for the tests (we mock the
+// access to tmdb), but it is executed as part of the instantiation of the
+// Injector class.
+process = process || {};
+process.env = process.env || {};
+process.env.apiKey = 'dummy';
