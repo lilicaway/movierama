fb6d0712cb9c80e24f8634585c51c76435885512
Liliana <liliana.nu@gmail.com>
2019-07-09 22:15:57 +0200

    Revert "Trying to see if heroku works if we deploy to build instead of dist."
    This wasn't the problem and Heroku now works.
    
    This reverts commit 9770d63de52e47953c4135180e6713f3e5d8733e.

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = env => {
     mode: 'development',
     entry: ['babel-polyfill', './src/index'],
     output: {
-      path: path.resolve(__dirname, 'build'),
+      path: path.resolve(__dirname, 'dist'),
       filename: 'app.bundle.js',
     },
     resolve: {
@@ -52,7 +52,7 @@ module.exports = env => {
       }),
     ],
     devServer: {
-      contentBase: __dirname + '/build',
+      contentBase: __dirname + '/dist',
       compress: true,
       port: 9000,
       open: true,
